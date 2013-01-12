b4c0ea8ec6b31e65e4d300ccb6a12b79f086fe74
Daniel Neuhäuser <dasdasich@gmail.com>
2013-01-12 09:40:41 +0100

    Add PBKDF2Hasher

---
+++ pwhash/hashers.py
@@ -0,0 +1,102 @@
+# coding: utf-8
+"""
+    pwhash.hashers
+    ~~~~~~~~~~~~~~
+
+    :copyright: 2013 by Daniel Neuhäuser
+    :license: BSD, see LICENSE.rst for details
+"""
+import os
+from collections import OrderedDict
+
+from pwhash.algorithms import pbkdf2
+from pwhash.utils import constant_time_equal
+
+
+DIGEST_SIZES = {
+    "hmac-sha1": 20,
+    "hmac-sha224": 28,
+    "hmac-sha256": 32,
+    "hmac-sha384": 48,
+    "hmac-sha512": 64
+}
+
+DEFAULT_SALT_LENGTH = 16 # 128 bit
+
+
+def generate_salt(salt_length):
+    return os.urandom(salt_length).encode("hex")
+
+
+def hexed_length(hexed):
+    return len(hexed.decode("hex"))
+
+
+class Hasher(object):
+    name = None
+
+    def parse(self, hash):
+        raise NotImplementedError()
+
+    def create(self, password):
+        raise NotImplementedError()
+
+    def verify(self, password, known_hash):
+        raise NotImplementedError()
+
+    def upgrade(self, password, known_hash):
+        raise NotImplementedError()
+
+    def verify_and_upgrade(self, password, known_hash):
+        matches = self.verify(password, known_hash)
+        if matches:
+            return matches, self.upgrade(password, known_hash)
+        return matches, None
+
+
+class PBKDF2Hasher(Hasher):
+    name = b"pbkdf2"
+
+    def __init__(self, rounds, method="hmac-sha1", salt_length=DEFAULT_SALT_LENGTH):
+        self.rounds = rounds
+        self.method = method
+        self.salt_length = salt_length
+        self.hash_length = DIGEST_SIZES[method]
+
+    def parse(self, hash):
+        if hash.startswith(self.name):
+            hash = hash[len(self.name) + 1:]
+        method, rounds, salt, hash = hash.split(b"$")
+        return {
+            "method": method,
+            "rounds": int(rounds),
+            "salt": salt,
+            "hash": hash
+        }
+
+    def create(self, password):
+        salt = generate_salt(self.salt_length)
+        hash = pbkdf2(password, salt, self.rounds, self.hash_length, self.method)
+        return b"$".join(
+            [self.name, self.method, bytes(self.rounds), salt, hash]
+        )
+
+    def verify(self, password, known_hash):
+        parsed = self.parse(known_hash)
+        hash = pbkdf2(
+            password,
+            parsed["salt"],
+            parsed["rounds"],
+            hexed_length(parsed["hash"]),
+            parsed["method"]
+        )
+        return constant_time_equal(hash, parsed["hash"])
+
+    def upgrade(self, password, known_hash):
+        parsed = self.parse(known_hash)
+        if (self.salt_length > hexed_length(parsed["salt"]) or
+            self.rounds > parsed["rounds"] or
+            self.hash_length > hexed_length(parsed["hash"]) or
+            DIGEST_SIZES[self.method] > DIGEST_SIZES[parsed["method"]]
+            ):
+            return self.create(password)
---
+++ pwhash/tests/test_hashers.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+"""
+    pwhash.tests.test_hashers
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: 2013 by Daniel Neuhäuser
+    :license: BSD, see LICENSE.rst for details
+"""
+from pwhash.hashers import PBKDF2Hasher
+
+
+def test_pbkdf2_hasher():
+    hasher = PBKDF2Hasher(1)
+    assert hasher.create(b"password") != hasher.create(b"password")
+
+    hash = hasher.create(b"password")
+    assert hasher.verify(b"password", hash)
+    assert not hasher.verify(b"other-password", hash)
+
+    upgraded = PBKDF2Hasher(2)
+    assert hasher.upgrade(b"password", hash) is None
+    assert upgraded.upgrade(b"password", hash) is not None
+
+    assert hasher.verify_and_upgrade(b"password", hash) == (True, None)
+    assert hasher.verify_and_upgrade(b"other-password", hash) == (False, None)
+    verified, new_hash = upgraded.verify_and_upgrade(b"password", hash)
+    assert verified
+    assert new_hash is not None
+
+    hash = upgraded.create(b"password")
+    assert hasher.verify(b"password", hash)
+    assert hasher.upgrade(b"password", hash) is None
+    assert hasher.verify_and_upgrade(b"password", hash) == (True, None)
